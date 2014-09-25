7f9aea1acfd1ef4fd7f84e7bbc1e3981e6a5bb28
Joel <batemanjoel4@gmail.com>
2014-09-25 01:45:53 -0700

    uncommented main files

--- Hmwk/Assignment4/LinkedList/LinkedList/main.cpp
+++ Hmwk/Assignment4/LinkedList/LinkedList/main.cpp
@@ -1,28 +1,28 @@
-////
-////  main.cpp
-////  LinkedList
-////
-////  Created by Joel Bateman on 9/24/14.
-////  Copyright (c) 2014 Joel Bateman. All rights reserved.
-////
 //
-////System Libraries
-//#include <iostream>
-//using namespace std;
+//  main.cpp
+//  LinkedList
 //
-////Our Library
-//#include "LnkdLst.h"
+//  Created by Joel Bateman on 9/24/14.
+//  Copyright (c) 2014 Joel Bateman. All rights reserved.
 //
-//int main(int argc, char** argv) {
-//    //Create a linked list
-//    LnkdLst list(0);
-//    //Append 3 more chains
-//    int clinks=3;
-//    for(int i=1;i<=clinks;i++){
-//        list.append(i);
-//    }
-//    //Print the data
-//    cout<<list.toString()<<endl;
-//    //Exit stage right!
-//    return 0;
-//}
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Our Library
+#include "LnkdLst.h"
+
+int main(int argc, char** argv) {
+    //Create a linked list
+    LnkdLst list(0);
+    //Append 3 more chains
+    int clinks=3;
+    for(int i=1;i<=clinks;i++){
+        list.append(i);
+    }
+    //Print the data
+    cout<<list.toString()<<endl;
+    //Exit stage right!
+    return 0;
+}
